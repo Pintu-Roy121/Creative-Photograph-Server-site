174dbb2ce15385f938c1b0228b9171b94a30d002
Pintu-Roy121 <roypintu121@gmail.com>
2022-11-08 19:01:39 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+
+
 const port = process.env.PORT || 5000;
 
 
@@ -12,14 +15,85 @@ app.use(express.json());
 
 const services = require('./services.json')
 
+
+// MongoDb database connnection..............
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.geiv5ao.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+
+        // collections ...............
+        const servicesCollections = client.db('CreativePhotograph').collection('services');
+        const reviewsCollections = client.db('CreativePhotograph').collection('reviews');
+
+
+        // get limit of services data ...........................
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollections.find(query).limit(3);
+            const services = await cursor.toArray()
+
+            res.send(services);
+        })
+        // get all services data.................................
+        app.get('/allservices', async (req, res) => {
+            const query = {}
+            const cursor = servicesCollections.find(query);
+            const allServices = await cursor.toArray();
+
+            res.send(allServices);
+        })
+
+        // get specific service using filtering with id.........................
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollections.findOne(query);
+
+            res.send(service);
+        })
+
+
+
+
+
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(error => console.log(error.message)
+)
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Server is working!')
 })
 
 
-app.get('/services', (req, res) => {
-    res.send(services)
-})
+// app.get('/services', (req, res) => {
+//     const limitService = services.slice(0, 3);
+//     res.send(limitService)
+// })
+
+// app.get('/allservices', (req, res) => {
+//     res.send(services);
+// })
+
+// app.get('/service/:id', (req, res) => {
+//     const id = req.params.id;
+//     const service = services.find(ser => ser.id === id)
+//     res.send(service)
+// })
 
 app.listen(port, () => {
     console.log(`server is runnign on port: ${port}`);
